af25d490481a092c01b5b4f0775d5d088d34288a
Hanan Marghoob <107300415+Human-100@users.noreply.github.com>
2025-01-15 15:57:21 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 
 import "./App.css"
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { useState,useEffect } from "react";
+import axios from 'axios'
 import { Link } from 'react-router-dom';
 
+
+
+
+const App = () => {
+    
+ 
     return (
         <>
 
