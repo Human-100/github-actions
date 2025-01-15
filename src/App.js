e103b20165933aea94806247b9b37f7c2b0effc4
Hanan Marghoob <107300415+Human-100@users.noreply.github.com>
2025-01-15 15:12:57 +0530

    homechanged

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home";
+import Home from "./home.js";
 import "./App.css";
 
 function App() {
